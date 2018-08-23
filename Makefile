0fcbf38b453b2f248528695d17317994b549d9ea
Dominic Ginger <domginger@hotmail.co.uk>
2018-08-23 14:48:23 +0100

    Add Makefile

---
+++ Makefile
@@ -0,0 +1,12 @@
+default:
+	go run main.go
+
+build:
+	rm -rf dist
+	mkdir dist
+	GOOS=linux go build main.go
+	mv main dist/
+	upx --brute dist/main
+
+deploy:
+	cd dist && now
--- main.go
+++ main.go
@@ -6,12 +6,14 @@ import (
     "net/http"
 )
 
+const port = "3003"
+
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         fmt.Fprintln(w, "Hello world")
     })
 
-    log.Println("Serving on localhost:3003")
-    err := http.ListenAndServe(":3003", nil)
+    log.Println("Serving on " + port)
+    err := http.ListenAndServe(":" + port, nil)
     log.Fatal(err)
 }
