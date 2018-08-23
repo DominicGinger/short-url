af8d5675f71ea3a30534e824e2bad610b1897250
Dominic Ginger <domginger@hotmail.co.uk>
2018-08-23 15:40:52 +0100

    Add an in memory map of key/values

--- main.go
+++ main.go
@@ -1,19 +1,78 @@
 package main
 
 import (
+    "crypto/rand"
     "fmt"
     "log"
     "net/http"
 )
 
 const port = "3003"
+const storageLimit = 5000
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Hello world")
+    data := make(map[string]string)
+
+    http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
+        params := r.URL.Query()
+        key := params.Get("key")
+        value := params.Get("value")
+
+        if (key == "") {
+            key = randomKey(2)
+        }
+
+        if (value == "") {
+            fmt.Fprintln(w, "Missing value param, expected ?value=y")
+            return
+        }
+        if (!validKey(key)) {
+            fmt.Fprintln(w, "Key too long, must be less than 16 chars")
+            return
+        }
+        if (!validValue(value)) {
+            fmt.Fprintln(w, "Value too long, must be less than 1000 chars")
+            return
+        }
+
+        if (len(data) >= storageLimit) {
+            data = make(map[string]string)
+        }
+
+        data[key] = value
+
+        fmt.Fprintln(w, key)
     })
 
-    log.Println("Serving on " + port)
+    http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+        params := r.URL.Query()
+        key := params.Get("key")
+
+        if (key == "") {
+            fmt.Fprintln(w, "Missing key param, expected ?key=x")
+            return
+        }
+
+        fmt.Fprintln(w, data[key])
+    })
+
+    log.Println("Listening on " + port)
     err := http.ListenAndServe(":" + port, nil)
     log.Fatal(err)
+}
+
+func validKey(key string) bool {
+    return len(key) <= 16
+}
+
+func validValue(value string) bool {
+    return len(value) <= 1000
+}
+
+func randomKey(length int) string {
+    b := make([]byte, length)
+    if _, err := rand.Read(b); err != nil {
+        panic(err)
+    }
+    return fmt.Sprintf("%X", b)
 }
